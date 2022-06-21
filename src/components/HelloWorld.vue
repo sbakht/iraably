53d1d9431bbb5dc204342ed3b1e70bfad138adda
Saad Bakht <rotk13@gmail.com>
2022-06-20 19:11:32 -0500

    cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -59,28 +59,6 @@ sub.setAnswers([1,3, 2])
 
   <div v-if="q.isDone">Points: {{q.points}}</div>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
