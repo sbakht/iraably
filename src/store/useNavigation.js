72dfef59b8d376bb63e51762d2c9aad82a6f8b6c
Saad Bakht <rotk13@gmail.com>
2022-07-06 18:00:50 -0500

    navigation tests

--- src/store/useNavigation.js
+++ src/store/useNavigation.js
@@ -1,84 +1,29 @@
-import { computed, ref, readonly } from "vue";
-import { defineStore } from "pinia";
-
-function getById(questions, id) {
-  return questions[id];
-}
+import { computed, ref, readonly, unref } from "vue";
+import { useTraversable } from "./useTraversable";
 
 export const useNavigation = (questions) => {
-  const currentId = ref(0);
 
-  const questionById = (id) => {
-    return getById(questions, id);
+  const answers = ref({});
+
+  const cloned = questions.map((q) => ({ ...q }));
+
+  const traversable = useTraversable(cloned);
+  const currentQuestion = traversable.currentItem;
+
+  const obj = {
+    ...traversable,
+    currentQuestion,
+    currentAnswer: computed(() => {
+      return answers.value[currentQuestion.value.id];
+    }),
+    saveAnswer(a) {
+      answers.value[currentQuestion.value.id] = a;
+    },
+    answers: readonly(answers)
   };
 
-  const currentQuestion = computed(() => {
-    return questionById(currentId.value);
-  });
+  delete obj.currentId;
+  delete obj.currentItem;
 
-  const isFinished = ref(false);
-
-  const finish = () => { isFinished.value = true }
-
-
-  const next = () => {
-    if (currentId.value >= questions.length - 1) {
-      finish();
-    } else {
-      currentId.value++;
-    }
-  };
-
-  const previous = () => {
-    if (quiz.value.currentId > 0) {
-      currentId.value--;
-    }
-  };
-
-  const reset = () => {
-    currentId.value = 0;
-    isFinished.value = false;
-  };
-
-  // const answers = ref({})
-
-  // const answer = (answerId) => {
-  //   answers.value[quiz.value.currentId] = answerId;
-  //   next();
-  // }
-
-  // const points = computed(() => {
-  //   return Object.keys(answers.value).map((questionId) => {
-  //     const question = questionById(questionId);
-  //     return question.correct === answers.value[questionId] ? 1 : 0;
-  //   }).reduce((x, accum) => x + accum, 0);
-  // })
-
-  // const formattedAnswers = computed(() => {
-  //   return Object.keys(answers.value).map((questionId) => {
-  //     const question = questionById(questionId);
-  //     return {
-  //       title: question.title,
-  //       answer: question.answers.find(a => a.id === answers.value[questionId])
-  //     }
-  //   })
-  // })
-
-  // const isDone = computed(() => {
-  //   return Object.keys(answers.value).length >= quiz.value.questions.length;
-  // });
-
-  return {
-    // quiz: readonly(quiz),
-    currentQuestion: readonly(currentQuestion),
-    next,
-    previous,
-    reset,
-    isFinished: readonly(isFinished)
-    // answer,
-    // answers,
-    // formattedAnswers,
-    // points,
-    // isDone
-  };
+  return obj;
 };
---
+++ src/store/useNavigation.test.js
@@ -0,0 +1,69 @@
+import { expect, test } from "vitest";
+import { useNavigation } from "./useNavigation";
+import { isReadonly } from "vue";
+
+const questions = [
+  {
+    id: 1, title: 'What is the day?', correct: [3], answers: [
+      { id: 1, title: 'Monday' },
+      { id: 2, title: 'Friday' },
+    ]
+  },
+  {
+    id: 3, title: 'What time is it?', correct: [4], answers: [
+      { id: 1, title: '12:00' },
+      { id: 2, title: '3:00' },
+      { id: 3, title: '5:00' },
+    ]
+  },
+  {
+    id: 7, title: 'What colors are cool?', correct: [1, 2, 4], answers: [
+      { id: 1, title: 'blue' },
+      { id: 2, title: 'green' },
+      { id: 3, title: 'red' },
+      { id: 4, title: 'orange' },
+    ]
+  }
+]
+
+test("exports traversable data/functions", () => {
+  const n = useNavigation(questions);
+  expect(n.currentId).toBeUndefined();
+  expect(n.currentItem).toBeUndefined();
+  expect(n.next).toBeTypeOf('function');
+  expect(n.previous).toBeTypeOf('function');
+  expect(n.reset).toBeTypeOf('function');
+  expect(n.isFinished.value).toBeTypeOf('boolean');
+  expect(n.isAtStart.value).toBeTypeOf('boolean');
+});
+
+test("current question", () => {
+  const n = useNavigation(questions);
+  expect(n.currentQuestion.value).toEqual(questions[0]);
+
+  n.next();
+  expect(n.currentQuestion.value).toEqual(questions[1]);
+});
+
+test("current answer", () => {
+  const n = useNavigation(questions);
+  expect(n.currentAnswer.value).toBe(undefined);
+
+  n.saveAnswer(2)
+  expect(n.currentAnswer.value).toEqual(2);
+
+  n.next();
+  expect(n.currentAnswer.value).toEqual(undefined);
+  n.saveAnswer([1, 3])
+  expect(n.currentAnswer.value).toEqual([1, 3]);
+
+  n.previous();
+  expect(n.currentAnswer.value).toEqual(2);
+
+  expect(n.answers.value).toEqual({
+    1: 2,
+    3: [1, 3],
+  })
+
+  expect(isReadonly(n.answers)).toBe(true)
+});
